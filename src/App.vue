796b57908abb77dc9960bf9311684650a4ee7f6f
Bilge <be@meo.io>
2024-06-13 22:23:17 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup>
 import { onMounted, ref } from 'vue';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const connection = ref(null);
 const message = ref('');
 const messages = ref([]);
-const userId = ref(uuidv4());
+const userId = uuidv4();
 
 onMounted(() => {
   connection.value = new WebSocket('wss://echo.websocket.org');
-
-  connection.value.onmessage = (event) => {
-    messages.value.push({ userId: userId, text: event.data });
-  };
-
   connection.value.onopen = () => {
     console.log('Connected to the WebSocket server');
   };
+
+  connection.value.onmessage = (event) => {
+    console.log('Received:', event.data);
+    messages.value.push({ userId, text: event.data });
+  };
+  
 
   connection.value.onclose = () => {
     console.log('Disconnected from the WebSocket server');
@@ -24,6 +25,7 @@ onMounted(() => {
 });
 
 function sendMessage() {
+  if(!message.value) return;
   connection.value.send(message.value);
   message.value = '';
 }
@@ -32,13 +34,17 @@ function sendMessage() {
 <template>
   <div>
     <h1>Real-Time Chat</h1>
-    <input type="text" v-model="message" @keyup.enter="sendMessage">
-    <ul>
+    <form @submit.prevent="sendMessage">
+    <input type="text" v-model="message">
+    <button>+</button>
+  </form>
+  <ul>
       <li v-for="message in messages" :key="message.id">
-        {{ message.text }}
+        {{ message }}
       </li>
     </ul>
   </div>
+  
 </template>
 
 <style scoped>
